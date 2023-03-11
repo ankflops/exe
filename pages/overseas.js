239ae58b7c6fc65cfc79281482acba1b0a7a7b5e
Ankhaa <ankhaab69@gmail.com>
2023-03-11 18:09:05 +0800

    mongolia

--- pages/overseas.js
+++ pages/overseas.js
@@ -53,6 +53,40 @@ const overseas = () => {
           </div>
         </div>
       </div>
+      <div className="pb-8">
+      <div className="flex justify-center">
+        <div className="grid md:grid-cols-2">
+          <div className=" p-4 m-4 md:p-4">
+            <h2 className=" text-3xl font-bold text-gray-800">
+            モンゴルプロジェクト
+            </h2>
+            <p className="my-8 text-xl font-medium text-gray-500">
+            2010年に日本初の投資事業会社Sanaa EXE LLCを設立し、不動産投資事業、金融事業、教育事業を展開しています。エクセプラザ、Olonglong」スクールを運営しています。
+
+            </p>
+            <div className="flex justify-center lg:justify-start">
+              <Link href="/mongolia">
+                <button
+                  className="bg-gray-200 px-14 py-3 bg-darkblue text-gray-400 font-extrabold text-base leading-snug uppercase"
+                  role="button"
+                  data-mdb-ripple="true"
+                  data-mdb-ripple-color="light"
+                >
+                  詳細はこちら
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="p-4">
+            <img
+              src="/assets/img/1659582198.jpg"
+              loading="lazy"
+              className="w-75% h-auto align-middle mx-auto bg-center bg-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
     </MainLayout>
   );
 };
